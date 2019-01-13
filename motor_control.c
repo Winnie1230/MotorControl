c8d8d03e52a56cd373c549895fa10a03ffc90773
Winnie1230 <winnie1998228@gmail.com>
2019-01-13 12:03:15 +0800

    rewrite encoder

--- motor_control.c
+++ motor_control.c
@@ -24,12 +24,13 @@ double duration;
 
 //encoder
 int encoder_pre;
-int encoder_count;
+int encoder_end;
 int encoder_check;
+int diff;
 struct timeval start, finish;
 
 //PI control
-double KP_1 , KP_2 , KI_1 , KI_2 , G_1 , G_2 , in_1 , in_2, ERR_1 , ERR_2;
+double KP_1 , KP_2 , KI_1 , KI_2 , G_1 , G_2 , in_1 , in_2;
 int PWM1, PWM2;
 
 void init(){
@@ -39,16 +40,16 @@ void init(){
 	
 	//encoder
 	encoder_pre = 0;
-	encoder_count = 0;
+	encoder_end = 0; //end flag
 	encoder_check = 0;
+	diff = -1;
 
 	//PI control
-	PWM1 = 600;		PWM2 = 600;
+	PWM1 = 800;		PWM2 = 800;
 	KP_1 = 300;		KP_2 = 500;
-	KI_1 = 0;		KI_2 = 0;
+	KI_1 = 1000;		KI_2 = 1000;
 	G_1 = 0;		G_2 = 0;
 	in_1 = 0; 		in_2 = 0;
-	ERR_1 = 0; 		ERR_2 = 0;
 
 	//car_speed
 	speed1 = 0;//motor1
@@ -58,13 +59,14 @@ void init(){
 
 void encoder(int);
 double car_speed(int);
-int pi_control(int,double,double);
+int pi_control(int , double , double);
 void sighandler(int);
 
 int main(){
 	if(wiringPiSetup()== -1){
 		exit(1);
 	}
+
 	init();
 	double vcmd1, vcmd2;
 	pinMode(ENCODER_PIN1, INPUT);
@@ -74,52 +76,38 @@ int main(){
 	pinMode(MOTOR1_PIN2, OUTPUT);
 	pinMode(MOTOR2_PIN2, OUTPUT);
 	
+	digitalWrite(MOTOR1_PIN2 , LOW);
+	digitalWrite(MOTOR2_PIN2 , HIGH);
+
 	signal(SIGINT, sighandler);
 
 	//printf("Enter vcmd1 vcmd2:");
 	//scanf("%lf %lf", &vcmd1, &vcmd2);
+	//printf("start\n");	
 
-	digitalWrite(MOTOR1_PIN2 , LOW);
-	digitalWrite(MOTOR2_PIN2 , HIGH);
-	
 	//pwmWrite(MOTOR1_PIN1 , 600);
 
 	//pwmWrite(MOTOR2_PIN1 , 600);
-	pwmWrite(MOTOR1_PIN1, 60);
+	/*pwmWrite(MOTOR1_PIN1, 60);
 	pwmWrite(MOTOR2_PIN1, 60);
-	delay(500);
-
-
+	delay(500);*/
 
 	while(1){
-		pwmWrite(MOTOR1_PIN1, 900);
-		//digitalWrite(MOTOR1_PIN2, HIGH);
-		pwmWrite(MOTOR2_PIN1, 900);
-		//digitalWrite(MOTOR2_PIN2 , LOW);
-	}
-
-	//while(1) ;
-	/*while(1){
 		pwmWrite(MOTOR1_PIN1, PWM1);
-		//printf("motor_1\n");
-		//usleep(1000);
 		pwmWrite(MOTOR2_PIN1, PWM2);
-		//printf("motor_2\n");
-		//usleep(3000);
 		//printf("start\n");
-		speed1 = car_speed(1);
+		//speed1 = car_speed(1);
 		//printf("speed1\n");
 		//delay(5);
-		speed2 = car_speed(2);
+		//speed2 = car_speed(2);
 		//printf("speed2\n");
-		//delay(5);
-		//printf("%lf %lf\n", speed1 , speed2);
+		//printf("%lf   %lf\n", speed1 , speed2);
 		//printf("current_speed2 = %lf\n", speed2);
-		PWM1 = pi_control(1 , 2, speed1);
-		PWM2 = pi_control(2 , 2, speed2);
-		printf("%lf %d %lf %d\n", speed1 , PWM1, speed2 , PWM2);
+		//PWM1 = pi_control(1 , 2, speed1);
+		//PWM2 = pi_control(2 , 2, speed2);
+		//printf("%lf %d %lf %d\n", speed1 , PWM1, speed2 , PWM2);
 		//printf("PWM2 = %d\n", PWM2);
-	}*/
+	}
 	return 0;
 }
 
@@ -130,18 +118,63 @@ void encoder(int pin){
 	else 
 		encoder_current = digitalRead(ENCODER_PIN2);
 	//printf("encoder_current = %d\n", encoder_current);
-	if(encoder_current != encoder_pre){
-		if(encoder_check == 0){
-			encoder_check = 1; //start to count
-			gettimeofday(&start , NULL);
+	/*if(encoder_current != encoder_pre){
+		if(encoder_check == 0){//not start to count
+			if(diff < 0)
+				diff = 0;
 		}
 		else{ //end to count
+			if(diff < 0)
+				diff = 0;
+		}
+
+		if(diff < 0)
+			diff = 0;
+	}
+	else if(encoder_current == encoder_pre){
+		if(diff >=0 && diff < 3)
+			diff ++;
+		else{
+			check = 1;
+			gettimeofday(&start , NULL);
+			diff = -1;
+		}
+	}*/
+	/*if(encoder_current != encoder_pre){
+		if (encoder_check == 0){
+			encoder_check = 1;
+			gettimeofday(&start , NULL);
+		}
+		else{
 			encoder_check = 0;
 			gettimeofday(&finish , NULL);
 		}
 	}
-	else if(encoder_current == encoder_pre && encoder_check == 1){
-		encoder_count ++;
+	else if(encoder_current == encoder_pre && encoder_check == 1)
+		encoder_count ++;*/
+
+	if(encoder_current != encoder_pre){
+		if(diff == -1)
+			diff = 0;
+		else if((diff >= 0 && diff < 4) && encoder_check == 1) //noise
+			diff = -1;
+	}
+	else if(encoder_current == encoder_pre){
+		if(diff >= 0 && diff < 4)
+			diff ++;
+		if(diff >= 4){
+			if(encoder_check == 0){
+				encoder_check = 1; //start to count
+				gettimeofday(&start , NULL);
+				diff = -1;
+			}
+			else{//encoder_check = 1
+				encoder_check = 0; //end counting
+				gettimeofday(&finish , NULL);
+				diff = -1;
+				encoder_end = 1;//end flag
+			}
+		}	
 	}
 	encoder_pre = encoder_current;
 }
@@ -150,34 +183,33 @@ double car_speed(int pin){
 	while(1){
 		encoder(pin);
 		usleep(sample_time);
-		if(encoder_count > 0 && encoder_check == 0)
+		if(encoder_end = 1 && encoder_check == 0)
 			break;
 	}
 
 	duration = (finish.tv_sec - start.tv_sec) + (finish.tv_usec - start.tv_usec) * 0.000001;
 
 	double speed = rad * 0.0675 / duration;
-	encoder_count = 0;
+	encoder_end = 0;
 	return speed;
 }
 
 int pi_control(int num , double vcmd, double speed){
 	int PWM = 0;
-	double G , in;
+	double ERR , G , in;
+	
+	ERR = vcmd - speed;
 
 	if(num == 1){
-		ERR_1 = vcmd - speed;
-		G_1 = ERR_1 * KP_1;
-		in_1 = in_1 + ERR_1 * duration * KI_1;
+		G_1 = ERR * KP_1;
+		in_1 = in_1 + ERR * duration * KI_1;
 		
 		G = G_1;
 		in = in_1;
-		
 	}
 	else{
-		ERR_2 = vcmd - speed;
-		G_2 = ERR_2 * KP_2;
-		in_2 = in_2 + ERR_2 * duration * KI_2;
+		G_2 = ERR * KP_2;
+		in_2 = in_2 + ERR * duration * KI_2;
 	
 		G = G_2;
 		in = in_2;
@@ -194,7 +226,7 @@ int pi_control(int num , double vcmd, double speed){
 		in = -1023;
 
 	PWM = G + in;
-	if(abs(PWM) >= 10230)
+	if(abs(PWM) >= 1023)
 		PWM = 1023;
 	else 
 		PWM = abs(PWM);
